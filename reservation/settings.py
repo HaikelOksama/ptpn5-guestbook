881e20f92835a7d5b295110bda8d64ced663921e
Oksama <95363039+HaikelOksama@users.noreply.github.com>
2022-12-16 19:42:25 +0700

    Update settings.py

--- reservation/settings.py
+++ reservation/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-dxt91%p&eam$tfb(b221x530igazbus&%^)(fyf)6!p3rg(+9$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
